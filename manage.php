04d2e70f46137a63578502889311e2e4b9343f07
Steven Schronk <steven@schronk.com>
2012-05-25 13:12:34 -0500

    Managment page has better look and feel.

--- manage.php
+++ manage.php
@@ -12,7 +12,6 @@ $result = mysql_query($sql);
 $row = mysql_fetch_row($result);
 $user_count = $row[0];
 
-
 $sql = 'select count(*), max(timeposted) from schedule';
 
 $result = mysql_query($sql);
@@ -26,7 +25,6 @@ $result = mysql_query($sql);
 $row = mysql_fetch_row($result);
 $comment_count = $row[0];
 $comment_max = $row[1];
-
 
 $sql = 'select count(*), max(timeposted) from filecom';
 
@@ -51,27 +49,37 @@ $std_locked = $row[0];
 			// ADIMINISTRATOR MENU
 ?>
 <div class="col2">
-	<h3><img src="gfx/rss.png">Feeds:</h3>
-		<a href="comment_feed.php">Full Comments</a><br>
-		<a href="assignment_feed.php">Assignments Modified</a>
 
-	<h3><img src="gfx/report.png">Assignments:</h3>
-		<a href="assignment_add.php">Add New Assignment</a><br>
-
-	<h3><img src="gfx/user_female.png">Users:</h3>
-		<a href="enrollment.php">Enrollment Manager</a><br>
-		<a href="enrollment_new.php">New Enrollment</a><br>
-		<a href="password_change.php">Change Personal Password</a><br>
-		<a href="password_change_user.php">Change User Password</a><br>
-
-	<h3><img src="gfx/report.png">Workflows:</h3>
-		<a href="index.php">Comments From Students</a><br>
-		<a href="workflow_comments.php">Most Recent Comments</a><br>
-		<a href="workflow_ungraded.php">Assignments Ready For Grading</a>
-
-	<h3><img src="gfx/database.png">System Admin:</h3>
-		<a href="adminer.php">SQL Admin</a><br>
-
+	<table>
+		<tr><td><img src="gfx/rss.png"></td><td class="link_header">Feeds</td></tr>
+		<tr><td></td><td><a href="comment_feed.php">Full Comments</a></td></tr>
+		<tr><td></td><td><a href="assignment_feed.php">Assignments Modified</a></td></tr>
+	</table>
+	<br><br>
+	<table>
+		<tr><td><img src="gfx/report.png"></td><td class="link_header">Assignments</td></tr>
+		<tr><td></td><td><a href="assignment_add.php">Add New Assignment</a></td></tr>
+	</table>
+	<br><br>
+	<table>
+		<tr><td><img src="gfx/user_female.png"></td><td class="link_header">Users</td></tr>
+		<tr><td></td><td><a href="enrollment.php">Enrollment Manager</a></td></tr>
+		<tr><td></td><td><a href="enrollment_new.php">New Enrollment</a></td></tr>
+		<tr><td></td><td><a href="password_change.php">Change Personal Password</a></td></tr>
+		<tr><td></td><td><a href="password_change_user.php">Change User Password</a></td></tr>
+	</table>
+	<br><br>
+	<table>
+		<tr><td><img src="gfx/cog.png"></td><td class="link_header">Workflows</td></tr>
+		<tr><td></td><td><a href="index.php">Comments From Students</a></td></tr>
+		<tr><td></td><td><a href="workflow_comments.php">Most Recent Comments</a></td></tr>
+		<tr><td></td><td><a href="workflow_ungraded.php">Assignments Ready For Grading</a></td></tr>
+	</table>
+	<br><br>
+	<table>
+		<tr><td><img src="gfx/database.png"></td><td class="link_header">System Admin</td></tr>
+		<tr><td></td><td><a href="adminer.php">SQL Admin</a></td></tr>
+	</table>
 </div>
 <div class="col">
 	<table class="gridtable">
